a9cd2df148decb3b671285fb4ccf4b16c30cb36d
suliar <gaucho4utd@hotmail.com>
2020-03-01 00:03:23 +0000

    Made changes to this service

--- main.go
+++ main.go
@@ -13,7 +13,7 @@ import (
 	pb "github.com/suliar/shippy-service-consignment/proto/consignment"
 )
 
-const port  = ":50051"
+const port = ":50051"
 
 type repository interface {
 	Create(*pb.Consignment) (*pb.Consignment, error)
@@ -21,21 +21,17 @@ type repository interface {
 }
 
 // Repository - Dummy repository, this simulates the use of a datastore
-// of some kind. We'll replace this with a real implementation later
-
+// of some kind. We'll replace this with a real implementation later on.
 type Repository struct {
 	consignments []*pb.Consignment
 }
 
-// Create a new consignment
 func (repo *Repository) Create(consignment *pb.Consignment) (*pb.Consignment, error) {
 	updated := append(repo.consignments, consignment)
 	repo.consignments = updated
 	return consignment, nil
-
 }
 
-// GetAll gets all consignments
 func (repo *Repository) GetAll() []*pb.Consignment {
 	return repo.consignments
 }
@@ -43,58 +39,54 @@ func (repo *Repository) GetAll() []*pb.Consignment {
 // Service should implement all of the methods to satisfy the service
 // we defined in our protobuf definition. You can check the interface
 // in the generated code itself for the exact method signatures etc
-// to give you better idea
+// to give you a better idea.
 type service struct {
 	repo repository
 }
 
-func (s *service) GetConsignments(ctx context.Context, req *pb.GetRequest, res *pb.Response) error {
-	consignments := s.repo.GetAll()
-
-	res.Consignments = consignments
-	return nil
-}
-
-// CreateConsignment - we created just one method on our service
-// which is just one method, which takes a context and a request as
-// an argument, these are handled by the grpc server
-
-
+// CreateConsignment - we created just one method on our service,
+// which is a create method, which takes a context and a request as an
+// argument, these are handled by the gRPC server.
 func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment, res *pb.Response) error {
 
 	// Save our consignment
 	consignment, err := s.repo.Create(req)
 	if err != nil {
-		return  err
+		return err
 	}
 
+	// Return matching the `Response` message we created in our
+	// protobuf definition.
 	res.Created = true
 	res.Consignment = consignment
 	return nil
 }
 
+func (s *service) GetConsignments(ctx context.Context, req *pb.GetRequest, res *pb.Response) error {
+	consignments := s.repo.GetAll()
+	res.Consignments = consignments
+	return nil
+}
+
 func main() {
+
 	repo := &Repository{}
-	// Create a New Service. Optionally include some options here.
+
+	// Create a new service. Optionally include some options here.
 	srv := micro.NewService(
-		// This name must match the package name given in your protobuf definitin
-		micro.Name("shippy.service.consignment"))
 
+		// This name must match the package name given in your protobuf definition
+		micro.Name("shippy.service.consignment"),
+	)
 
-	//Init will parse the command line flag
+	// Init will parse the command line flags.
 	srv.Init()
 
+	// Register handler
+	pb.RegisterShippingServiceHandler(srv.Server(), &service{repo})
 
-
-	// Register the handler
-	if err := pb.RegisterShippingServiceHandler(srv.Server(), &service{repo}); err !=nil {
-		fmt.Println(err)
-	}
-
-
-	// run the server
+	// Run the server
 	if err := srv.Run(); err != nil {
 		fmt.Println(err)
 	}
-
 }
